d74642c53c1d0db1dbf5fc43a0e43cbe960ced90
unsnipeable <mtnkylolomg@gmail.com>
2025-07-13 17:02:56 +0900

    概要の編集

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -6,13 +6,13 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
 @Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
 public class ExampleMod {
-    public static final String MODID = "2muuclient2";
-    public static final String VERSION = "2.0";
+    public static final String MODID = "maphider";
+    public static final String VERSION = "1.0";
 
     public static boolean maphider = true;
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
-        System.out.println("2muu client");
+        System.out.println("2muu OP");
     }
 }
